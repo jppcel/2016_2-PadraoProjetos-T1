830ddbe6fa156a504eebcd87938c1fe49336d793
João Paulo Polles <joaopaulopolles@gmail.com>
2016-09-05 20:41:16 -0300

    Criado enum Genero

---
+++ bancotads/src/main/java/br/univel/bancotads/enums/Genero.java
@@ -0,0 +1,23 @@
+package br.univel.bancotads.enums;
+
+public enum Genero {
+	MASCULINO(1,"Masculino"),
+	FEMININO(2,"Feminino"),
+	NAODEFINIDO(3,"Não Definido");
+	
+	int id;
+	String genero;
+	
+	private Genero(int id, String genero){
+		this.id = id;
+		this.genero = genero;
+	}
+	
+	public String getGenero(){
+		return this.genero;
+	}
+	
+	public int getId(){
+		return this.id;
+	}
+}
