dde6c6b28766ed272f07374605707710e8d11b52
João Paulo Polles <joaopaulopolles@gmail.com>
2016-09-27 20:07:58 -0300

    Alterada a possibilidade de algumas coisas

--- bancotads/src/main/java/br/univel/bancotads/view/ClientPasswordView.java
+++ bancotads/src/main/java/br/univel/bancotads/view/ClientPasswordView.java
@@ -4,13 +4,24 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.border.EmptyBorder;
+
+import br.univel.bancotads.Login;
+
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
+import javax.swing.JSeparator;
 
 public class ClientPasswordView extends JFrame {
 
@@ -20,6 +31,7 @@ public class ClientPasswordView extends JFrame {
 	private static final long serialVersionUID = -7334626293156765251L;
 	private JPanel contentPane;
 	private JPasswordField textField;
+	private JLabel lb_operacao;
 	
 	private void addValue(int value){
 		if(this.textField.getDocument().getLength() < 6){
@@ -31,25 +43,50 @@ public class ClientPasswordView extends JFrame {
 	 * Create the frame.
 	 */
 	public ClientPasswordView(final DefaultView dv) {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent e) {
+				dv.setEnabled(true);
+			}
+		});
+		setResizable(false);
+		setAlwaysOnTop(true);
 		setTitle("Senha Operações - Banco TADS");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 418, 144);
+		setBounds(100, 100, 418, 163);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
 		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
 		gbl_contentPane.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
+		
+		lb_operacao = new JLabel("[OPERAÇÃO]");
+		lb_operacao.setFont(new Font("Dialog", Font.BOLD, 18));
+		GridBagConstraints gbc_lb_operacao = new GridBagConstraints();
+		gbc_lb_operacao.gridwidth = 7;
+		gbc_lb_operacao.insets = new Insets(0, 0, 5, 0);
+		gbc_lb_operacao.gridx = 0;
+		gbc_lb_operacao.gridy = 0;
+		contentPane.add(lb_operacao, gbc_lb_operacao);
+		
+		JSeparator separator = new JSeparator();
+		GridBagConstraints gbc_separator = new GridBagConstraints();
+		gbc_separator.gridwidth = 7;
+		gbc_separator.insets = new Insets(0, 0, 5, 5);
+		gbc_separator.gridx = 0;
+		gbc_separator.gridy = 1;
+		contentPane.add(separator, gbc_separator);
 		
 		JLabel lblDigiteSuaSenha = new JLabel("Digite sua senha utilizando o teclado virtual");
 		GridBagConstraints gbc_lblDigiteSuaSenha = new GridBagConstraints();
 		gbc_lblDigiteSuaSenha.gridwidth = 7;
-		gbc_lblDigiteSuaSenha.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDigiteSuaSenha.insets = new Insets(0, 0, 5, 0);
 		gbc_lblDigiteSuaSenha.gridx = 0;
-		gbc_lblDigiteSuaSenha.gridy = 0;
+		gbc_lblDigiteSuaSenha.gridy = 2;
 		contentPane.add(lblDigiteSuaSenha, gbc_lblDigiteSuaSenha);
 		
 		textField = new JPasswordField();
@@ -59,7 +96,7 @@ public class ClientPasswordView extends JFrame {
 		gbc_textField.insets = new Insets(0, 0, 5, 5);
 		gbc_textField.fill = GridBagConstraints.BOTH;
 		gbc_textField.gridx = 0;
-		gbc_textField.gridy = 1;
+		gbc_textField.gridy = 3;
 		contentPane.add(textField, gbc_textField);
 		textField.setColumns(10);
 		
@@ -72,7 +109,7 @@ public class ClientPasswordView extends JFrame {
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
 		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
 		gbc_btnNewButton.gridx = 2;
-		gbc_btnNewButton.gridy = 1;
+		gbc_btnNewButton.gridy = 3;
 		contentPane.add(btnNewButton, gbc_btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("1");
@@ -84,7 +121,7 @@ public class ClientPasswordView extends JFrame {
 		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
 		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 5);
 		gbc_btnNewButton_1.gridx = 3;
-		gbc_btnNewButton_1.gridy = 1;
+		gbc_btnNewButton_1.gridy = 3;
 		contentPane.add(btnNewButton_1, gbc_btnNewButton_1);
 		
 		JButton button = new JButton("2");
@@ -96,7 +133,7 @@ public class ClientPasswordView extends JFrame {
 		GridBagConstraints gbc_button = new GridBagConstraints();
 		gbc_button.insets = new Insets(0, 0, 5, 5);
 		gbc_button.gridx = 4;
-		gbc_button.gridy = 1;
+		gbc_button.gridy = 3;
 		contentPane.add(button, gbc_button);
 		
 		JButton button_1 = new JButton("3");
@@ -108,7 +145,7 @@ public class ClientPasswordView extends JFrame {
 		GridBagConstraints gbc_button_1 = new GridBagConstraints();
 		gbc_button_1.insets = new Insets(0, 0, 5, 5);
 		gbc_button_1.gridx = 5;
-		gbc_button_1.gridy = 1;
+		gbc_button_1.gridy = 3;
 		contentPane.add(button_1, gbc_button_1);
 		
 		JButton button_2 = new JButton("4");
@@ -120,15 +157,29 @@ public class ClientPasswordView extends JFrame {
 		GridBagConstraints gbc_button_2 = new GridBagConstraints();
 		gbc_button_2.insets = new Insets(0, 0, 5, 0);
 		gbc_button_2.gridx = 6;
-		gbc_button_2.gridy = 1;
+		gbc_button_2.gridy = 3;
 		contentPane.add(button_2, gbc_button_2);
 		
 		JButton btnConfirmar = new JButton("Confirmar");
+		btnConfirmar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				final Login l = dv.getL();
+				Boolean ok = l.checkSenhaOperacoes(String.copyValueOf(textField.getPassword()));
+				if(ok){
+					dv.setEnabled(true);
+					setVisible(false);
+				}else{
+					JOptionPane.showMessageDialog(dv, "Senha incorreta!");
+				}
+				textField.setText("");
+			}
+		});
 		GridBagConstraints gbc_btnConfirmar = new GridBagConstraints();
 		gbc_btnConfirmar.anchor = GridBagConstraints.WEST;
 		gbc_btnConfirmar.insets = new Insets(0, 0, 0, 5);
 		gbc_btnConfirmar.gridx = 0;
-		gbc_btnConfirmar.gridy = 2;
+		gbc_btnConfirmar.gridy = 4;
 		contentPane.add(btnConfirmar, gbc_btnConfirmar);
 		
 		JButton button_3 = new JButton("5");
@@ -147,12 +198,12 @@ public class ClientPasswordView extends JFrame {
 		GridBagConstraints gbc_btn_limpar = new GridBagConstraints();
 		gbc_btn_limpar.insets = new Insets(0, 0, 0, 5);
 		gbc_btn_limpar.gridx = 1;
-		gbc_btn_limpar.gridy = 2;
+		gbc_btn_limpar.gridy = 4;
 		contentPane.add(btn_limpar, gbc_btn_limpar);
 		GridBagConstraints gbc_button_3 = new GridBagConstraints();
 		gbc_button_3.insets = new Insets(0, 0, 0, 5);
 		gbc_button_3.gridx = 2;
-		gbc_button_3.gridy = 2;
+		gbc_button_3.gridy = 4;
 		contentPane.add(button_3, gbc_button_3);
 		
 		JButton button_4 = new JButton("6");
@@ -164,7 +215,7 @@ public class ClientPasswordView extends JFrame {
 		GridBagConstraints gbc_button_4 = new GridBagConstraints();
 		gbc_button_4.insets = new Insets(0, 0, 0, 5);
 		gbc_button_4.gridx = 3;
-		gbc_button_4.gridy = 2;
+		gbc_button_4.gridy = 4;
 		contentPane.add(button_4, gbc_button_4);
 		
 		JButton button_5 = new JButton("7");
@@ -176,7 +227,7 @@ public class ClientPasswordView extends JFrame {
 		GridBagConstraints gbc_button_5 = new GridBagConstraints();
 		gbc_button_5.insets = new Insets(0, 0, 0, 5);
 		gbc_button_5.gridx = 4;
-		gbc_button_5.gridy = 2;
+		gbc_button_5.gridy = 4;
 		contentPane.add(button_5, gbc_button_5);
 		
 		JButton button_6 = new JButton("8");
@@ -188,7 +239,7 @@ public class ClientPasswordView extends JFrame {
 		GridBagConstraints gbc_button_6 = new GridBagConstraints();
 		gbc_button_6.insets = new Insets(0, 0, 0, 5);
 		gbc_button_6.gridx = 5;
-		gbc_button_6.gridy = 2;
+		gbc_button_6.gridy = 4;
 		contentPane.add(button_6, gbc_button_6);
 		
 		JButton button_7 = new JButton("9");
@@ -199,7 +250,10 @@ public class ClientPasswordView extends JFrame {
 		});
 		GridBagConstraints gbc_button_7 = new GridBagConstraints();
 		gbc_button_7.gridx = 6;
-		gbc_button_7.gridy = 2;
+		gbc_button_7.gridy = 4;
 		contentPane.add(button_7, gbc_button_7);
+	}
+	public void setOperacao(String operacao){
+		lb_operacao.setText(operacao);
 	}
 }
